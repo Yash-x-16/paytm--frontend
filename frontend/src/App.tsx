5cd5741af70bf7da392bf92eceb172e0a15d258b
Yash Mishra <mishrayash392@gmail.com>
2025-04-19 11:19:12 +0530

    added password input box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,12 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import './App.css'
 import { InputC } from './components/inputC' 
 import { UserIcon } from './icons/userIcon'
-
+import { PasswordC } from './components/password'
 function App() {
  
   return (<div>
     <InputC type='placeholder' label='USERNAME' startIcon={<UserIcon size='sm'/>} endIcon={<UserIcon size="sm"/>}></InputC>
-    <InputC type='placeholder' label='firstname' startIcon={<UserIcon size='sm'/>} endIcon={<UserIcon size="sm"/>}></InputC>
-    <InputC type='placeholder' label='lastname' startIcon={<UserIcon size='sm'/>} endIcon={<UserIcon size="sm"/>}></InputC>
-    <InputC type='password' label='password' startIcon={<UserIcon size='sm'/>} endIcon={<UserIcon size="sm"/>}></InputC>
+   <PasswordC/>
     </div>
   )
 }
--- frontend/src/components/password.tsx
+++ frontend/src/components/password.tsx
@@ -1,26 +1,36 @@
 import { ReactElement, Ref } from "react";
-import { UserIcon } from "../icons/userIcon"; 
+import { LockIcon } from "../icons/lockIcons"; 
+import { EyeIcon } from "../icons/eyeIcon";
 
 
 interface input{
-    type :string ,
-    label : string , 
-    startIcon?:ReactElement,
+    type ?:string ,
 refrence? : Ref<HTMLInputElement> , 
 endIcon? :ReactElement 
 }
 
 
-export function InputC(prop:input){
-    return <div className="h-12 w-10 bg-slate-500 flex rounded-md m-7 justify-start" > 
-        <div className="pt-3 pl-2 rounded-md  ">
-            {prop.startIcon}
-            </div> 
-            <div className="bg-inherit pt-3 pl-2 rounded-md text-gray-100 ">
-                    <input type={prop.type}  
-                        placeholder={prop.label}  
-                        ref={prop.refrence} 
-                        className=" bg-inherit focus:outline-none "/>
-            </div>
+export function PasswordC(prop:input){
+    return (
+  <div className="flex justify-start bg-slate-500 rounded-md m-7 h-12 w-10">
+    {/* Lock Icon */}
+    <div className="text-white pt-3  pl-2">
+      <LockIcon />
     </div>
+
+    {/* Input Field */}
+    <input
+      type={prop.type}
+      placeholder="PASSWORD"
+      ref={prop.refrence}
+      className="bg-inherit  focus:outline-none w-40 inline-block pl-2"
+    />
+
+    {/* Eye Icon */}
+    <button type="button" className=" pr-2 bg-inherit pt-3 mr-3 inline-flex ">
+      <EyeIcon />
+    </button>
+  </div>
+);
+
 }
